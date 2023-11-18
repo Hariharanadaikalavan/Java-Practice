331c1b0da012e21eb2b4512b28d2b258e075fbf9
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-18 21:34:51 +0530

    Create RightAngleTriangle.java

---
+++ PatternPrinting/RightAngleTriangle.java
@@ -0,0 +1,17 @@
+public class RightAngleTriangle {
+    public static void main(String args[]){
+        int i,j;
+       for(i=1;i<=5;i++)
+       {
+           for(j=i;j<=5;j++){
+               System.out.print(" ");
+           }
+           for( i=1;i<=j;i++){
+           System.out.print("*");
+       }
+       
+       System.out.println(" ");
+    }
+    }
+    
+}
