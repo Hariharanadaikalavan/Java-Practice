38ada314449c4b75f62654d8e4596d806dad3252
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-18 21:35:18 +0530

    Create Leftangletriangle.java

---
+++ PatternPrinting/Leftangletriangle.java
@@ -0,0 +1,14 @@
+public class Leftangletriangle {
+    public static  void main(String args[])
+    {
+       for(int j=3;j>0;j--){
+        for(int i=j;i>0;i--)
+        {
+            System.out.print(" ");
+        }
+        for(int i=1;i<=j;i++){
+        System.out.println("* ");
+        }
+       }
+}
+}
