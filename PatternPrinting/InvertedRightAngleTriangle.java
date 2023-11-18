b435ed22758c81924e83414853a599ef402114cd
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-18 21:35:53 +0530

    Create InvertedRightAngleTriangle.java

---
+++ PatternPrinting/InvertedRightAngleTriangle.java
@@ -0,0 +1,20 @@
+public class InvertedRightAngleTriangle {
+    public static void main(String args[]){
+        for(int j=3;j>0;j--){
+        for(int i=j;i>0;i--)
+        {
+            System.out.print("* ");
+        }
+        System.out.println(" ");
+        }
+        
+        for(int j=0;j<=5;j++){
+        for(int i=j;i<=5;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.println(" ");
+        }
+        
+    }
+}
