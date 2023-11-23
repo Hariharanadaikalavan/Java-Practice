cc48f357845cf0697422ebd263cf92dfec3a8913
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-23 22:47:57 +0530

    Create 5.MainMethodDeclaration.java

---
+++ InterviewQuestions/5.MainMethodDeclaration.java
@@ -0,0 +1,13 @@
+public class MainMethodDeclaration {
+    public static void main(String args[]){
+        print();
+    }
+    public static void print(double... a){
+        System.out.println("Double");
+    }
+    public static void print(int... a){
+        System.out.println("Int");
+    }
+}
+   
+    
