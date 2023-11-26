bee780213326e0f3545bc39333d00031a8c738bb
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-26 22:13:50 +0530

    Create 6.LinkedHashSet.java

---
+++ Collection/6.LinkedHashSet.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashSet;
+import java.util.Itertor;
+import java.util.ArrayList;
+
+class LinkedHashSet{
+public static void main(String args[]){
+LinkedList<String>obj=new LinkedList<String>();
+obj.add("hariharan");
+
+Itertor<String>itr=obj.itertor();
+while(itr.hasNext(){
+System.out.println(itr.next());
+}
+}
+}
