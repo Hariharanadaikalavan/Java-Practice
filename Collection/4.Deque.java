e8c7708412f34bb89ef9752eab640263c6332c8c
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-26 22:12:15 +0530

    Create 4.Deque.java

---
+++ Collection/4.Deque.java
@@ -0,0 +1,17 @@
+
+import java.util.Iterator;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Dummy1 {
+    public static void main(String args[]) {
+        Deque<String> myDeque = new ArrayDeque<>();
+        myDeque.add("Revathi");
+        myDeque.add("Revathi");
+        myDeque.add("Revathi");
+
+        for (String str : myDeque) {
+            System.out.println(str);
+        }
+    }
+}
