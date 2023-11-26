4e8e127e3f97ab98364a0ef7992bfab73098266e
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-26 22:09:42 +0530

    Create 1.LinkedListCollection.java

---
+++ Collection/1.LinkedListCollection.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class LinkedListCollection{
+public static void main(String hari[]){
+      LinkedList<String> obj=new LinkedList<String>();
+      obj.add("Hariharan");
+
+Iterator<String> obj1=obj.iterator();
+while(obj1.hasNext()){
+System.out.println(obj);
+}
+}
+}
