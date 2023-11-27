794de528f30e909b6bd13700a19b91ed97006fb5
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-27 23:42:27 +0530

    Create 6.HigherCommonFactor.java

---
+++ InterviewQuestions/6.HigherCommonFactor.java
@@ -0,0 +1,21 @@
+public class HigherCommonFactor {
+    public static void main(String args[]){
+        int num1=12,num2=30;
+        
+        
+        int str=(num1<num2)?num1:num2;
+        //System.out.println("Largest Number is:"+str);
+        
+        while(str>2){
+        if((num1%str==0)&&(num2%str==0))
+        {
+            System.out.println("hcf="+str);
+            break;
+            
+        }
+        str--;
+        }
+        
+        
+    }
+}
