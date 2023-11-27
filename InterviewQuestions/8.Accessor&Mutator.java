eacba812c73f42818d32f393b98eeee5534e2955
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-27 23:44:29 +0530

    Create 8.Accessor&Mutator.java

---
+++ InterviewQuestions/8.Accessor&Mutator.java
@@ -0,0 +1,55 @@
+class Dummy{
+private String username;
+private int age;
+private String email;
+private long phoneNumber;
+
+public String getUsername(){
+return username;
+}
+
+public void setUsername(String username){
+this.username=username;
+}
+
+public int getAge(){
+return age;
+}
+
+public void setAge(int age){
+this.age=age;
+}
+
+public String getEmail(){
+return email;
+}
+
+public void setEmail(String email){
+this.email=email;
+}
+
+public long getPhoneNumber(){
+return phoneNumber;
+}
+
+public void setPhoneNumber(long phoneNumber){
+    this.phoneNumber = phoneNumber;
+}
+
+public String tostring(){
+    String str=getUsername();
+            return str;
+}
+
+
+public static void main(String args[]) {
+    final Dummy obj = new Dummy();
+    obj.setUsername("Hariharan");
+    obj.setAge(23);
+    obj.setEmail("@gmail.com");
+    obj.setPhoneNumber(989447);
+
+    System.out.println(obj.tostring());
+
+}
+}
