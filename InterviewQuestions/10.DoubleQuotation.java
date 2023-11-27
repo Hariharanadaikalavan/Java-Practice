5a045960738fc69be52e99a1d6342c19d95cd8fc
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-27 23:51:27 +0530

    Create 10.DoubleQuotation.java

---
+++ InterviewQuestions/10.DoubleQuotation.java
@@ -0,0 +1,19 @@
+public class Semicolon {
+    public static void main(String args[])
+    {
+        boolean flag=true;
+        if(flag)
+        {
+            System.out.println("\"Hello\""+"<---True Block Excuted");
+        }else{
+                System.out.println("Bye");
+                }
+         boolean flags=false;
+        if(flags)
+        {
+            System.out.println("\"Hello\"");
+        }else{
+                System.out.println("False block is excuted--->"+"\"Bye\"");
+                }
+    }
+}
