39002c2c739a67b25c790c171be73a98dff99339
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-29 19:52:24 +0530

    Create 15.Increment.java

---
+++ InterviewQuestions/15.Increment.java
@@ -0,0 +1,17 @@
+public class incrementDecrement {
+    public static void main(String args[]){
+        int x=0,y=10;
+        do{
+            y--;
+            ++x;
+        }while(x<5);
+        System.out.println(x+" ,"+y);
+ System.out.println(x);
+         System.out.println(++x);
+          System.out.println(x);
+           System.out.println(y);
+          System.out.println(y--);
+           System.out.println(y);
+         
+    }
+}
