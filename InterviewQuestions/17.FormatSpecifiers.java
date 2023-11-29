078b4d633e84e380aa09161ad6c1fa4d184cf4b3
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-29 19:57:17 +0530

    Create 17.FormatSpecifiers.java

---
+++ InterviewQuestions/17.FormatSpecifiers.java
@@ -0,0 +1,20 @@
+public class Semicolon {
+    public static void main(String args[])
+    {
+        boolean flag=true;
+        if(flag)
+        {
+            System.out.println("\"Hello\""+"<---True Block Excuted");
+        }else{
+                System.out.println("Bye");
+                }
+         boolean flags=false;
+        if(flags)
+        {
+            System.out.println("\"Hello\"");
+        }else{
+                System.out.println("False block is excuted--->"+"\"Bye\"");
+                }
+    }
+}
+//----------->\" "\
