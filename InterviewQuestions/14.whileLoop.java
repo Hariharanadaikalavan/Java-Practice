90d74811b900589b8a7d95d33e7d578009e4d80b
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-29 19:51:27 +0530

    Create 14.whileLoop.java

---
+++ InterviewQuestions/14.whileLoop.java
@@ -0,0 +1,10 @@
+public class whileLoop {
+ public static void main(String args[]){
+     String a="";
+     while(a.length()!=2)
+        
+     a=a+"a";
+     System.out.println(a);
+     
+ }   
+}
