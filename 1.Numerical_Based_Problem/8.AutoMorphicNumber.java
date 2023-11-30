b6dc49a09d6a5fb79508fee9672ffae206fd63cf
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-30 23:08:29 +0530

    Create 8.AutoMorphicNumber.java

---
+++ 1.Numerical_Based_Problem/8.AutoMorphicNumber.java
@@ -0,0 +1,27 @@
+public class AutoMorphicNumber {
+    public static void main(String args[]){
+       int Original=25;
+       int Number=Original;
+       int Power=Original*Original;
+       int PowerRemainder=Power;
+       int Flag=0;//625
+       while(Number!=0)
+       {
+                int Remainder=Number%10;
+                         PowerRemainder=Power%10;
+                if(Remainder!=PowerRemainder){
+                 Flag=1;
+        }       
+                 Number=Number/10; 
+                         Power=Power/10;
+       }
+               
+               System.out.println(PowerRemainder+"-"+Original);
+               
+               if(Flag==0){
+                   System.out.println("AutoMorphicNumber");
+               }else{
+                   System.out.println("The number did satify the Conditions");
+               }
+    }
+}
