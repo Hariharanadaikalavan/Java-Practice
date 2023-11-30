7fd5087ce220d5d5e720960c8f9034eb552d524c
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-30 21:12:07 +0530

    Create 7.NeonNumber.java

---
+++ 1.Numerical_Based_Problem/7.NeonNumber.java
@@ -0,0 +1,19 @@
+public class NeonNumber_6 {                  //input=9------->9*9=81--->8+1=9 sum of digits of square is equal to input number
+public static void main(String args[]){
+    int Original=9;
+    int Power=Original*Original;
+    int Number=Power;   //81
+    int Sum=0;
+    while(Number!=0)
+    {
+           int Remainder=Number%10;
+           Sum=Sum+Remainder;  //9
+           Number=Number/10;
+    }
+    
+    System.out.println(Sum+"-"+Power+"-"+Original);
+    
+    boolean Result=(Original==Sum);
+    System.out.println(Result);
+}    
+}
