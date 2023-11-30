f9c3f2df9c7b1ab1b8683a694e5876c277e62960
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-30 20:07:44 +0530

    Create 3.SpyNumber.java

---
+++ 1.Numerical_Based_Problem/3.SpyNumber.java
@@ -0,0 +1,18 @@
+public class SpyNumber_3 {                      //input =123---> 1+2+3=1*2*3 --->6==6
+    public static void main(String args[]){
+        int Original=123;
+        int Number=Original,Sum=0,Product=1;
+        while(Number!=0)
+        {
+                 int remainder=Number%10;
+                 Sum=Sum+remainder;
+                 Product=Product*remainder;
+                 Number=Number/10;
+        }
+        System.out.println(Sum+"-"+Product);
+        
+        boolean Result=(Sum==Product);
+        
+        System.out.println(Result);
+    }
+}
