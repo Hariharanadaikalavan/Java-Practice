135aa2264a5fd31830044f9eb32e88a23ba5b654
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-01 22:13:51 +0530

    Create 23.OctualNumber.java

---
+++ InterViewQuestion-II/23.OctualNumber.java
@@ -0,0 +1,8 @@
+public class OctalNumber {
+     public static void main(String args[]){
+        int x=08;  //Compile time error occur beacuse octal number can hold upto 0-7 values respectively
+        x=x+2;
+        System.out.println(x);
+    }
+    
+}
