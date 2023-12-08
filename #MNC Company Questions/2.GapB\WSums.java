83c56ecc13aa15d4a84899a58c36d7fa189fd63b
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-08 23:54:04 +0530

    Create 2.GapB\WSums.java

---
+++ #MNC Company Questions/2.GapB\WSums.java
@@ -0,0 +1,16 @@
+public class GapSum {
+    public static void main(String args[]){
+        int sum=0;
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        int gap=4;
+        
+        for(int j=0;j<gap;j++){
+        
+        for(int i=j;i<9;i=i+gap){
+            sum=sum+arr[i];
+        }
+        
+        System.out.print(sum+",");
+        }
+    }
+}
