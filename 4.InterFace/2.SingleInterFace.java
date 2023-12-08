e3c884e5b2ed5be4b94097fd63936d77476f3fd6
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-08 10:29:34 +0530

    Create 2.SingleInterFace.java

---
+++ 4.InterFace/2.SingleInterFace.java
@@ -0,0 +1,32 @@
+interface SmartPhone{
+    void call();
+    void browser();
+    void takePhone();
+    void videoCall();
+}
+
+
+
+
+public class InterFace implements SmartPhone {
+    public void call(){
+        System.out.println("call");
+    }
+    public void browser(){
+        System.out.println("browser");
+    }
+    public void takePhone(){
+        
+        System.out.println("takephone");
+    }
+    public void videoCall(){
+        System.out.println("videocall");
+    }
+    
+ 
+    public static void main(String args[]){
+        InterFace obj=new InterFace();
+        obj.call();
+        obj.browser();
+    }
+}
