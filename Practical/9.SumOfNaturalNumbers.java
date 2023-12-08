da662ed2cf45876afd902e1dd5df88037a16916c
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-08 21:22:18 +0530

    Create 9.SumOfNaturalNumbers.java

---
+++ Practical/9.SumOfNaturalNumbers.java
@@ -0,0 +1,28 @@
+public class sumofnatural {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+      String choice;
+      do
+      {
+          int i,sum=0,n;
+        System.out.println("Enter the number you want to sumupto from zero");
+        n=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(i%2!=0)
+            {
+            sum=sum+i;
+            }
+              
+        }
+           System.err.println("Sum of" +n+ "Natural Number  "    +sum);
+            choice=sc.nextLine();
+            System.out.println("Do you want to Continue");
+            
+      }
+      
+      while(choice.equals("yes"));
+    }
+    
+}
