9654a1bfef0a7110287ff40d0a4d08ddb92909a7
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-09 23:11:24 +0530

    Update 2.GapB\WSums.java

--- #MNC Company Questions/2.GapB\WSums.java
+++ #MNC Company Questions/2.GapB\WSums.java
@@ -2,7 +2,7 @@ public class GapSum {
     public static void main(String args[]){
         int sum=0;
         int arr[]={1,2,3,4,5,6,7,8,9};
-        int gap=4;
+        int gap=2;
         
         for(int j=0;j<gap;j++){
         
