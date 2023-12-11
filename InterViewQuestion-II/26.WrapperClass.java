5fc2e524615dcc0c437df17c59e719ca6230caac
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-11 22:49:42 +0530

    Create 26.WrapperClass.java

---
+++ InterViewQuestion-II/26.WrapperClass.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void print(int a) {
+    	System.out.println("int: " + a);
+	}
+ 	public static void print(Integer a) {
+    	System.out.println("Integer: " + a);
+	}
+ 
+	public static void main(String[] args) {
+    	int a = 5;
+    	Integer b = 10;
+    	print(a);
+    	print(b);
+	}
+}
