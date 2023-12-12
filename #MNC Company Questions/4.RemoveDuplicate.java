08ca707e984c3fbc19aa3860d9c8036b1a436d8c
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-12 19:57:52 +0530

    Create 4.RemoveDuplicate.java

---
+++ #MNC Company Questions/4.RemoveDuplicate.java
@@ -0,0 +1,14 @@
+public class RemoveDuplicate {
+    public static void main(String args[]){
+       int arr[]={1,3,3,3,3,3,3,4,4,6,6,6,6,7,7,7,8,8}; 
+        int j=1;
+       for(int i=0;i<arr.length;i++)
+       {
+                 if(arr[i]!=arr[j])
+                 {
+                     arr[j]=arr[i];
+                    System.out.println(arr[j]);
+                 }
+       }       
+  }
+}
