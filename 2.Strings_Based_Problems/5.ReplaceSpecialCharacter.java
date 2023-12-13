f76b7f835ceb0318a854691aa3a0644e6defc826
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-13 22:27:31 +0530

    Create 5.ReplaceSpecialCharacter.java

---
+++ 2.Strings_Based_Problems/5.ReplaceSpecialCharacter.java
@@ -0,0 +1,19 @@
+public class Replace {
+    public static void main(String args[]){
+       String str="hariharan";
+      char ch[]=str.toCharArray();
+             for(int i=0;i<str.length();i++)
+             {
+                 if(ch[i]=='a'|| ch[i]=='e'||ch[i]=='i' ||ch[i]=='o'||ch[i]=='u'){
+                 ch[i]='$';
+             }
+
+
+               
+       for(int i=0;i<str.length();i++)
+       {
+       System.out.print(ch[i]);
+       }
+       
+    }
+}
