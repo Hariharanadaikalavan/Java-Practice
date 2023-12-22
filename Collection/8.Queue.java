db577b0c01483880a9f0b151e4d25cab0be10073
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-22 20:07:40 +0530

    Create 8.Queue.java

---
+++ Collection/8.Queue.java
@@ -0,0 +1,17 @@
+public class QueueList {
+    
+    public static void main(String args[]){
+        
+        Queue<Integer> obj=new LinkedList<Integer>();
+        obj.add(20);
+        obj.add(50);
+    
+   System.out.print(obj);
+   
+  System.out.print(obj.peek()) ;
+  
+  System.out.println(obj.poll());
+    System.out.print(obj.peek()) ;
+   
+           }
+}
