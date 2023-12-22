b90b720c18d34e57583cf02d6f845eb2f34e94a0
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-22 19:38:09 +0530

    Update 7.ArrayList.java

--- Collection/7.ArrayList.java
+++ Collection/7.ArrayList.java
@@ -1,18 +1,20 @@
-import java.util.*;
+import java.util.*;  
+class ArrayListEx{  
+public static void main(String args[]){  
+ArrayList list=new ArrayList(); 
+list.add("Ravi");  
+list.add("Vijay");  
+list.add("Ravi");  
+list.add("Ajay");  
+list.add(23);
+list.add(true);
+list.set(2, "hari");
+System.out.println(list);
+System.out.print(list.get(2));
+Iterator itr=list.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}  
 
-class ArrayList{
-
-public static void main(String args[]){
-ArrayList<String> obj=new ArrayList<String>();
-obj.add("Hariharan");
-obj.add("");
-obj.add("Hariharan");
-System.out.println(obj);
-
-Iterator itr=obj.iterator();
-while(itr.hasNext()){
-System.out.println(itr.next());
-}
-
-}
-}
