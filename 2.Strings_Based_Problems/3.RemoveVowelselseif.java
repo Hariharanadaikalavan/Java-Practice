c4661a4267a8531bdb6fb415caf3ad6eb5a28278
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-25 23:43:06 +0530

    Update 3.RemoveVowelselseif.java

--- 2.Strings_Based_Problems/3.RemoveVowelselseif.java
+++ 2.Strings_Based_Problems/3.RemoveVowelselseif.java
@@ -1,7 +1,7 @@
 public class RemoveVowels {
     public static void main(String args[]){
        String str="hariharan";
-      char ch[]=str.toCharArray();
+      char ch[]=str.toCharArray(); // Convert into char array 
        for(int i=0;i<str.length();i++)
              {
                  if(ch[i]=='a'|| ch[i]=='e'||ch[i]=='i' ||ch[i]=='o'||ch[i]=='u'){
