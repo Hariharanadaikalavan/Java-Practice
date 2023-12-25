3c89ee22a0fe7765b83c4e737e6d108865ccded2
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-25 23:39:39 +0530

    Update 7.BiggestLetter.java

--- 2.Strings_Based_Problems/7.BiggestLetter.java
+++ 2.Strings_Based_Problems/7.BiggestLetter.java
@@ -3,7 +3,7 @@ public class BiggestLetter {
        
        String str="hariharan";
        char ch[]=str.toCharArray();
-       char large=ch[0];
+       char large=ch[0]; //fixed the first as largest
        for(int i=0;i<ch.length;i++)
        {
           if(ch[i]<large)
