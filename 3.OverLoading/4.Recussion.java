c1959cc2c22e6bfa5292f9d823b3757ce593232c
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-27 23:44:22 +0530

    Update 4.Recussion.java

--- 3.OverLoading/4.Recussion.java
+++ 3.OverLoading/4.Recussion.java
@@ -2,17 +2,17 @@
 {
     void method(String s1)
     {
-        method(s1, s1 + s1);//
+        method(s1, s1 + s1);  //java1 ,java1 
     }
  
     void method(String s1, String s2)
     {
-        method(s1, s2, s1+s2);//
+        method(s1, s2, s1+s2);//java1 ,java1 ,java1java1
     }
  
     void method(String s1, String s2, String s3)
     {
-        System.out.println(s1+s2+s3);
+        System.out.println(s1+s2+s3);//java1java1java1java1
     }
 }
  
