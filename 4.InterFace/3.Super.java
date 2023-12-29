dcb3ef0b0f141769cc0d45197acf49b6d29791b6
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-29 22:56:03 +0530

    Update 3.Super.java

--- 4.InterFace/3.Super.java
+++ 4.InterFace/3.Super.java
@@ -1,20 +1,20 @@
   class A
 {
-    {
+   A() {
         System.out.println(1);
     }
 }
  
 class B extends A
 {
-    {
+   B() {
         System.out.println(2);
     }
 }
  
 class C extends B
 {
-    {
+    C(){
         System.out.println(3);
     }
 }
