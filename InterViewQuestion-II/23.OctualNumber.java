9eba93f6de755a778848768852a39899200fb1f5
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2024-01-02 21:52:08 +0530

    Update 23.OctualNumber.java

--- InterViewQuestion-II/23.OctualNumber.java
+++ InterViewQuestion-II/23.OctualNumber.java
@@ -1,4 +1,4 @@
-public class OctalNumber {
+public class OctalNumber {  
      public static void main(String args[]){
         int x=08;  //Compile time error occur beacuse octal number can hold upto 0-7 values respectively
         x=x+2;
