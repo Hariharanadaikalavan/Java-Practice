a58b107899f3a83b6f9c64db7b399b8c8fd77ff9
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2024-01-13 23:03:54 +0530

    Update InvertedRightAngleTriangle.java

--- PatternPrinting/InvertedRightAngleTriangle.java
+++ PatternPrinting/InvertedRightAngleTriangle.java
@@ -1,6 +1,6 @@
 public class InvertedRightAngleTriangle {
     public static void main(String args[]){
-        for(int j=3;j>0;j--){
+        for(int j=3;j>0 ;j--){
         for(int i=j;i>0;i--)
         {
             System.out.print("* ");
