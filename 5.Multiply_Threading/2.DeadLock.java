b3997b26b92f3ff6ec589fc77fd97714fbb71071
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2024-01-31 23:59:54 +0530

    Update 2.DeadLock.java

--- 5.Multiply_Threading/2.DeadLock.java
+++ 5.Multiply_Threading/2.DeadLock.java
@@ -11,7 +11,7 @@ public class DeadlockExample {
                      Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }
+                }  
 
                 System.out.println("Thread 1: Waiting for lock 2");
                 synchronized (lock2) {
